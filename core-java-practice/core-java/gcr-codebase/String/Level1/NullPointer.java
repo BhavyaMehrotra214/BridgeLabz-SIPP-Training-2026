bbd8c14ecc41ad4dd247fb9b05a7ac2d786c195b
BhavyaMehrotra214 <bhavya.bm12@gmail.com>
2026-06-17 15:44:28 +0530

    added String level1 problems

---
+++ core-java-practice/core-java/gcr-codebase/String/Level1/NullPointer.java
@@ -0,0 +1,19 @@
+public class NullPointer{
+    static void generateException() {
+        String text = null;
+        System.out.println(text.length());
+    }
+
+    static void handleException() {
+        String text = null;
+
+        try {
+            System.out.println(text.length());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException Handled");
+        }
+    }
+    public static void main(String[] args) {
+        handleException();
+    }
+}
